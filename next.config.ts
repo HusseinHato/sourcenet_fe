0ddecdb6398f6a1be39b6139400dd9324f65afb7
HusseinHato <riantonaruto@gmail.com>
2025-11-24 02:19:23 +0700

    Fix Next Config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
+        destination: 'http://103.150.196.240:3001/:path*',
       },
     ];
   },
