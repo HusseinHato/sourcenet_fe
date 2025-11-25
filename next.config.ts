b6097842bcb5d465f0215450ec7133daea146712
HusseinHato <riantonaruto@gmail.com>
2025-11-25 21:12:22 +0700

    Fix Next Config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/proxy/:path*',
-        destination: 'http://103.150.196.240:3001/:path*',
+        source: '/api/:path*',
+        destination: 'http://api.pramadani.site/api/:path*',
       },
     ];
   },
